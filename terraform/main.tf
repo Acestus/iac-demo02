a89a32bdae31145bdcc404cdb7e1738132b24523
Acestus <architect@acestus.com>
2024-08-02 11:59:08 -0500

    chore: Update backend configuration in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 1.5.7"
   backend "azurerm" {
-    resource_group_name  = "thomasthorntoncloud"
-    storage_account_name = "thomasthorntontfstate"
-    container_name       = "github-thomasthorntoncloud-terraform-example"
-    key                  = "github-thomasthorntoncloud-terraform-example.tfstate"
+    resource_group_name  = "tamopstfstates"
+    storage_account_name = "gitops01skopos"
+    container_name       = "tfstatedevops"
+    key                  = "tfstatedevops.tfstate"
   }
 }
 
