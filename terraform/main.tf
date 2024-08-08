0f4d30e27318f6137a78758587b5376d3e6aa3bb
William Weeks-Balconi <wweeks@skopos.us>
2024-08-08 16:02:00 -0500

    &

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,4 +21,19 @@ resource "azurerm_resource_group" "tamops" {
   location = "westus2"
 }
 
+#Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tamops-vnet"
+  address_space       = ["192.168.0.0/16"]
+  location            = "uksouth"
+  resource_group_name = azurerm_resource_group.tamops.name
+}
+
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.tamops.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["192.168.0.0/24"]
+}
   
